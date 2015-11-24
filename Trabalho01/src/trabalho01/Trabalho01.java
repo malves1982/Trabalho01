696714f389f0a5f6036942134708881f00ffe713
malves <malves@inspiron-win>
2015-11-23 21:21:16 -0300

    Commit inicial do projeto; criação de janela;

---
+++ Trabalho01/src/trabalho01/Trabalho01.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho01;
+
+import java.awt.Frame;
+
+/**
+ *
+ * @author malves
+ */
+public class Trabalho01 extends Frame{
+
+    public Trabalho01() {
+        setVisible(true);
+        setSize(300, 300);
+    }
+
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Trabalho01 t = new Trabalho01();
+    }
+    
+}
